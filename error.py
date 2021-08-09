0ffb296bd1df5afa1915fce49c4b773d19ddd991
Cláudio <claudioduarterodrigues@gmail.com>
2021-08-09 16:21:21 +0100

    Refactor error script

--- error.py
+++ error.py
@@ -1,22 +1,25 @@
 import shapely.geometry as geom
+import matplotlib.pyplot as plt
 import numpy as np
 
 def calculate(ground_truth_x, ground_truth_y, estimated_x, estimated_y):
     """Calculates the average error margin for a test"""
 
     # Apply steps points to every Ground Truth line
-    points = np.linspace(start=ground_truth_x, stop=ground_truth_y, num=10)
+    points = np.linspace(start=ground_truth_x, stop=ground_truth_y, num=100)
     total_points = []
-
+    xs, ys= [], []
     # Save all points into an array of (x,y)
     for i in range(len(points) - 1):
         for j in range(len(ground_truth_x)):
             total_points.append((points[0][j],points[i+1][j]))
             total_points.append((points[i+1][j],points[0][j]))
-
+            xs.append(points[0][j])
+            ys.append(points[i+1][j])
+    plt.scatter(xs,ys)
+    plt.scatter(ys,xs)
     # Create a Shapely LineString from totality of Ground truth points
     ground_truth = geom.LineString(total_points)
-    print(ground_truth)
 
     # For every estimated point calculate the distance to the closest neighbor in the Ground Truth LineString
     for i in range(len(estimated_x)):
@@ -24,6 +27,6 @@ def calculate(ground_truth_x, ground_truth_y, estimated_x, estimated_y):
         estimated_point = geom.Point(estimated_x[i],estimated_y[i])
 
         # Calculate the distance from the estimated sample point to ground truth
-        closest_point = estimated_point.distance(ground_truth)
-        # point_on_line = line.interpolate(line.project(point))
-        # plt.plot([point.x, point_on_line.x], [point.y, point_on_line.y])
+        closest_distance = estimated_point.distance(ground_truth)
+        closest_point = ground_truth.interpolate(ground_truth.project(estimated_point))
+        plt.plot([estimated_point.x, closest_point.x], [estimated_point.y, closest_point.y])
