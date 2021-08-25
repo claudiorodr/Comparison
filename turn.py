0eed40b80a4f53fa6549d02fe75949fb3b9f56c4
Claudio <claudioduarterodrigues@gmail.com>
2021-08-25 12:09:38 +0100

    Add script to calculate turning points in trajectory

---
+++ turn.py
@@ -0,0 +1,10 @@
+from rdp import rdp
+import matplotlib.pyplot as plt
+
+
+points = []
+def trajectory(x, y):
+    """Estimates path trajectory using rdp algorithm"""
+    for i in range(len(x)):
+        points.append((x[i][0],y[i][0]))  
+    return rdp(points,epsilon=0.5)
